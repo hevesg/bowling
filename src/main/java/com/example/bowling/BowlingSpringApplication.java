870bd6d17dba27cbe09dc7532bb9f9476cf89272
Gabor Heves <hevesg@gmail.com>
2021-07-19 19:39:46 +0200

    initial commit

---
+++ src/main/java/com/example/bowling/BowlingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingSpringApplication.class, args);
+    }
+
+}
